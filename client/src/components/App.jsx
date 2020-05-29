04f8d7fd715bf746470bc970d4fa9549d0202b47
Sahil <pss991019@yahoo.in>
2020-05-29 17:56:03 +0530

    initialized React

---
+++ client/src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App=()=><div>App works</div>
+
+export default App;
--- server/routes/internship.js
+++ server/routes/internship.js
@@ -3,10 +3,17 @@ const handle = require('../handlers');
 const auth = require('../middlewares/auth');
 
 
-router.route('/').get(handle.showInternships).post(auth, handle.addNewInternship);
+router.route('/')
+.get(handle.showInternships)
+.post(auth, handle.addNewInternship);
 
-router.get('/student',auth,handle.studentsInternships);
-router.route('/:id').get(handle.getInternship).delete(auth, handle.deleteInternship);
+router
+.get('/student',auth,handle.studentsInternships);
+
+router
+.route('/:id')
+.get(handle.getInternship)
+.delete(auth, handle.deleteInternship);
 
 
 
