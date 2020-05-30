a09f083348cffe01799e58d29d02b08ece4c7fa3
Sahil <pss991019@yahoo.in>
2020-05-30 16:41:26 +0530

    register login finshed

--- client/src/containers/RouteViews.jsx
+++ client/src/containers/RouteViews.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Auth from '../components/Auth';
-//import Auth_2 from '../components/Auth_2';
+import Auth_2 from '../components/Auth_2';
 const RouteViews=()=>(<main>
 <Switch>
     <Route exact path='/login' render={() => <Auth authType="login"/>}/>
-    <Route exact path='/register' render={() => <Auth authType='register'/>}/>
+    <Route exact path='/register' render={() => <Auth_2 authType='register'/>}/>
 </Switch>
 </main>);
 
