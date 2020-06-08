397093aef8a13d56123946cb3ae8f516ef3150fa
Sahil <pss991019@yahoo.in>
2020-06-08 11:30:05 +0530

    accept reject buttons scrollbar

--- client/src/components/internships.jsx
+++ client/src/components/internships.jsx
@@ -13,40 +13,61 @@ class Internships extends Component{
     }
 
     render(){
+        var p=0;
         
        // const {auth,getInternships,getStudentInternships} =this.props;
         const internships=this.props.internships.map(internship => 
-        <tr   key={internship._id}>
-            <td>{internship.docs.ApplicationStatus}</td>
-            <td>{internship.docs.UndertakingStatus}</td>
-            <td>{internship.docs.OfferLetterStatus}</td>
-            <td>{internship.docs.MarksheetsStatus}</td>
-            <td>{internship.docs.AttendanceStatus}</td>
-            <td>{internship.approvedBy}</td>
-            <td>{internship.completionStatus}</td>
-            <td>{internship.holder}</td>
-            <td>{internship.student}</td>
+            
+        <tr   key={internship._id} style={{height:"120px"}}>
+            <td><b>{internship.docs.ApplicationStatus}</b></td>
+            <td><b>{internship.docs.UndertakingStatus}</b></td>
+            <td><b>{internship.docs.OfferLetterStatus}</b></td>
+            <td><b>{internship.docs.MarksheetsStatus}</b></td>
+            <td><b>{internship.docs.AttendanceStatus}</b></td>
+            <td><b>{internship.approvedBy}</b></td>
+            <td><b>{internship.completionStatus}</b></td>
+            
+            <td><b>{internship.student}</b></td>
             {/* <td><pre>{JSON.stringify(internship.application.submittedDate,null,2)}</pre></td> */}
-            <td>{new Date(internship.application.submittedDate).toLocaleDateString()}</td>
-            <td>{new Date(internship.application.approvedDate).toLocaleDateString()}</td>
-            <td>{internship.application.workplace}</td>
-            <td>{internship.application.durationOfInternship}</td>
-            <td>{internship.application.reference}</td>
-            <td>{internship.application.offerLetter}</td>
-            {/* <td>{internship.__v}</td> */}
+            <td><b>{new Date(internship.application.submittedDate).toLocaleDateString()}</b></td>
+            <td><b>{new Date(internship.application.approvedDate).toLocaleDateString()}</b></td>
+            <td><b>{internship.application.workplace}</b></td>
+            <td><b>{internship.application.durationOfInternship}</b></td>
+            <td><b>{internship.application.reference}</b></td>
+            <td><b>{internship.application.offerLetter}</b></td>
+            <td>
+                <form>
+                <input type="checkbox" className="accept" value="accept" id={"accept"+p} name="accept"/>
+                <label className="accept" htmlFor={"accept"+p}>ACCEPT</label>
+                
+                <input type="checkbox" className="reject" value="reject" id={"reject"+p} name="reject"/>
+                <label className="reject" htmlFor={"reject"+p}>REJECT</label>
+                </form>
+               
+            </td>
+           {p++}
+            
         </tr>);
 
 
     return <Fragment>
-       
+       <div style={{textAlign:"center"},{marginTop:"20px"}}>
+          <h3>
+            Internship Detais
+            <hr />
+          </h3>
+          
+        </div>
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
-            <div className="card" style={{margin:"2rem"}}>
+            
+            <div className="card" style={{margin:"3rem"}}>
         <table  className="table">
             <thead className="thead-dark">
                 <tr>
-                    <th colSpan="5" style={{border:"2px solid white"},{textAlign:"center"}}>DOCUMENT DETAILS</th>
-                    <th colSpan="4"></th>
+                    <th colSpan="5" style={{border:"1px solid white"},{textAlign:"center"}}>DOCUMENT DETAILS</th>
+                    <th colSpan="3"></th>
                     <th colSpan="6" style={{border:"1px solid white"},{textAlign:"center"}}>APPLICATION DETAILS</th>
+                    <th colSpan="1"></th>
                 </tr>
                 <tr>
                 <th >Application Status</th>
@@ -56,7 +77,7 @@ class Internships extends Component{
                 <th>Attendance Status</th>
                 <th>Approved By</th>
                 <th>Completion Status</th>
-                <th>Holder</th>
+               
                 <th>Student</th>
                 <th>Submitted Date</th>
                 <th>Approved Date</th>
@@ -64,6 +85,7 @@ class Internships extends Component{
                 <th>Duration Of Internship</th>
                 <th>Reference</th>
                 <th>OfferLetter</th>
+                <th>Approve/Reject</th>
                 </tr>
             </thead>
         {internships}</table></div></Fragment>
