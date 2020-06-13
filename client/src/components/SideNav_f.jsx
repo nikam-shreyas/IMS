70102fb2941407e7cb894704b020475ead036575
Sahil <pss991019@yahoo.in>
2020-06-13 10:56:36 +0530

    i am back

---
+++ client/src/components/SideNav_f.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { MdPermIdentity } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { MdDns } from "react-icons/md";
+import { MdDescription } from "react-icons/md";
+import { MdAddCircle } from "react-icons/md";
+
+class Sidenav_f extends Component {
+  state = {};
+  render() {
+    const activeNow = this.props.activeComponent;
+    return (
+      <div className="sidenav">
+        <p>Menu</p>
+        <ul>
+          <Link to="/faculty">
+            <li className={activeNow == 1 ? "sidenav-active" : ""}>
+              <span className="mx-2">
+                <MdDns />
+              </span>
+              Internship Details
+            </li>
+          </Link>
+          <Link to="/studentprofile">
+            <li className={activeNow == 2 ? "sidenav-active" : ""}>
+              <span className="mx-2">
+                <MdPermIdentity />
+              </span>
+              Profile
+            </li>
+          </Link>
+          <Link to="/apply">
+            <li className={activeNow == 3 ? "sidenav-active" : ""}>
+              <span className="mx-2">
+                <MdAddCircle />
+              </span>
+              Apply
+            </li>
+          </Link>
+          <li className={activeNow == 4 ? "sidenav-active" : ""}>
+            <span className="mx-2">
+              <MdDescription />
+            </span>
+            Documents
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Sidenav_f;
--- client/src/pages/Faculty_Home.js
+++ client/src/pages/Faculty_Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import {Redirect} from 'react-router-dom';
 import Internships from '../components/internships';
+import Sidenav_f from '../components/SideNav_f';
 import Auth from '../components/Auth';
 import ErrorMessage from '../components/ErrorMessage';
 
@@ -8,7 +9,12 @@ import ErrorMessage from '../components/ErrorMessage';
 const Faculty_Home=({authType,isAthenticated})=>{
 
 return <Fragment>
-    <Internships/>
+    <div className="col-sm-2 sidenav" style={{float:"left"}}>
+            <Sidenav_f activeComponent="1" />
+          </div>
+          <div className="col-sm-10 of">
+    <Internships style={{float:"left"}}/>
+    </div>
 </Fragment>
 }
 
