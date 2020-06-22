92b66e79547550d5ed3daeadd17ff845b8328792
Pralhad789 <pralhad207@gmail.com>
2020-06-22 19:18:04 +0530

    Login UI

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import image from "./assets/Pictlogo.jpeg";
+import style from "./assets/style.css"
 //import Auth_Page from '../pages/Auth_Page';
 import {
   authUser,
@@ -44,7 +46,7 @@ class Auth extends Component {
   render() {
     const { username, password } = this.state;
     return (
-      <div className="container">
+      /*<div className="container">
         <div className="card mx-auto my-5">
           <div className="card-body">
             <h2 className="card-title">Login</h2>
@@ -95,7 +97,7 @@ class Auth extends Component {
                   </option>
                 </select>
               </div>
-              {/* <Auth_Page User_type={this.state.User_type}/> */}
+              {/* <Auth_Page User_type={this.state.User_type}/> }    //comment this
 
               <div className="button_center">
                 <button className="btn btn-dark" type="submit">
@@ -105,7 +107,59 @@ class Auth extends Component {
             </form>
           </div>
         </div>
+      </div> */
+      <section>
+      <div class="container">
+          <div class="user signinBx">
+            <div class="imgBx"><img src={image}/></div>
+            <div class="formBx">
+              <form onSubmit={this.handleSubmit}>
+                <h2>Sign In</h2>
+                <input 
+                className="input"
+                type="text"
+                value={username}
+                name="username"
+                placeholder="Username"
+                autoComplete="off"
+                className="form-control"
+                onChange={this.handleChange} />
+
+                <input
+                className="input"
+                type="password"
+                value={password}
+                name="password"
+                placeholder="Password"
+                className="form-control"
+                autoComplete="off"
+                onChange={this.handleChange} /> 
+                
+                <select
+                  name="User_type"
+                  defaultValue="1"
+                  onChange={this.handleChange}
+                  className="form-control"
+                  id="exampleFormControlSelect1"
+                >
+                  <option name="student" value="1">
+                    STUDENT
+                  </option>
+                  <option name="faculty" value="2">
+                    FACULTY
+                  </option>
+                  <option name="admin" value="3">
+                    ADMIN
+                  </option>
+                </select>
+
+                <input type="submit" value="Login"/>
+                <p class="signup"><a href="#ForgotPassword">Forgot password?</a></p>
+              </form>
+            </div>
+          </div>
       </div>
+      </section>
     );
   }
 }
