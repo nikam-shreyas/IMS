855450812c2a203e698ad36ad60e6cce45754998
ZeronP <nik4m.5hreyas@gmail.com>
2020-07-19 14:06:01 +0530

    attempting to pull

--- client/src/pages/Analytics.js
+++ client/src/pages/Analytics.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from "react";
 // import 'office-ui-fabric-react/dist/css/fabric.css';
 // import '../App.css';
 
-import FinancialCharts from './AnalyticsCharts';
-import Admin_Sidenav from '../components/Admin_Sidenav';
+import AnalyticsCharts from "./AnalyticsCharts";
+import Admin_Sidenav from "../components/Admin_Sidenav";
 
-const Analytics= () =>{
-return(  
-  <div>
-  <div className="row no-gutters">
-    <div className="col-sm-2 sidenav">
-    <Admin_Sidenav activeComponent="5" />
-    </div>
-          <div className="col-sm-10">
-            <div className="container-fluid mt-2">
+const Analytics = () => {
+  return (
+    <div>
+      <div className="row no-gutters">
+        <div className="col-sm-2 sidenav">
+          <Admin_Sidenav activeComponent="5" />
+        </div>
+        <div className="col-sm-10">
+          <div className="container-fluid mt-2">
             <div className="main-element ms-Grid-col ms-sm10 ms-xl10">
-            <div className="ms-Grid-row">            
-                    <h3 style={{color:'black'}}>Monthly Application Analysis</h3>           
+              <div className="ms-Grid-row">
+                <h3 style={{ color: "black" }}>Monthly Application Analysis</h3>
               </div>
               <div className="ms-Grid-row">
-                <FinancialCharts />
+                <AnalyticsCharts />
               </div>
             </div>
-    </div>          
+          </div>
         </div>
       </div>
     </div>
-);
-}
+  );
+};
 
 export default Analytics;
